5437e603332f28fa1ba4fb23a7f91aae2e8a2d72
Emily Johnson <ejohnson@Emis-MacBook-Pro-3.local>
2014-01-18 14:25:18 -0500

    second commit

--- Quiz/QuizInClassViewController.m
+++ Quiz/QuizInClassViewController.m
@@ -14,16 +14,65 @@
 
 @implementation QuizInClassViewController
 
-- (void)viewDidLoad
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    // Call the init method implemented by the superclass
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Create two arrays and make the pointers point to them
+        questions = [[NSMutableArray alloc] init];
+        answers = [[NSMutableArray alloc] init];
+    
+        // Add questions and answers to the arrays
+        
+        [questions addObject:@"What is 7 + 7?"];
+        [answers addObject:@"14"];
+    
+        [questions addObject:@"What is the capital of Vermont?"];
+        [answers addObject:@"Montpelier"];
+    
+        [questions addObject:@"From what is cognac made?"];
+        [answers addObject:@"Grapes"];
+    }
+    // Return the address of the new object
+    return self;
 }
 
-- (void)didReceiveMemoryWarning
+
+
+- (IBAction)showQuestion:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    // Step to the next question
+    currentQuestionIndex++;
+    
+    // Am I past the last question?
+    if (currentQuestionIndex == [questions count]) {
+        
+        // Go back to the first question
+        currentQuestionIndex = 0;
+    }
+    
+    // Get the string at the index in the questions array
+    NSString *question = [questions objectAtIndex:currentQuestionIndex];
+    
+    // Log the string to the console
+    NSLog(@"displaying question: %@", question);
+    
+    // Display the string in the question field
+    [questionField setText:question];
+    
+    // Clear the answer field
+    [answerField setText:@"???"];
+    
+}
+- (IBAction)showAnswer:(id)sender
+{
+    // What is the answer to the current question?
+    NSString *answer =[answers objectAtIndex:currentQuestionIndex];
+    
+    // Display it in the answer field
+    [answerField setText:answer];
+    
 }
 
 @end
